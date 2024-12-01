123b879f6b18f71c958ccb01d350c9287cec59b4
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2024-11-30 19:05:13 -0800

    print out a book report

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ def main():
     text_content = get_text_from_path(book_path)
     word_count = get_word_count(text_content)
     char_count = get_character_count(text_content)
-    print(f"Book: {book_path}")
-    print(f"Word Count: {word_count}")
-    print(f"Char Count: {char_count}")
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    
+    char_list = get_sorted_by_count_filter_by_alpha(char_count)
+    for char_dict in char_list:
+        print(f"The '{char_dict["character"]}' character was found {char_dict["count"]} times")
+
+def sort_on_count(input):
+    return input["count"]
+
+def get_sorted_by_count_filter_by_alpha(char_count):
+    char_count_list = []
+    for char in char_count:
+        if char.isalpha():
+            char_count_list.append({"character": char, "count": char_count[char]})
+    char_count_list.sort(reverse=True, key=sort_on_count)
+    return char_count_list
 
 def get_character_count(text):
     char_count = {}
